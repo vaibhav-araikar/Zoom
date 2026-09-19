a45b4ae84a7f572025a7aa667d2057d540f29c12
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-09-20 00:32:26 +0530

    working on socket

--- Backend/src/controllers/socketManager.js
+++ Backend/src/controllers/socketManager.js
@@ -8,6 +8,29 @@ let timeOnline = {};
 
 const connectToSocketServer = (server) => {
   const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {
+      if (connections[path] === undefined) {
+        connections[path] = [];
+      }
+      connections[path].push(socket.id);
+      timeOnline[socket.id] = new Date(0);
+
+      for (let a = 0; a < connections[path].length; a++) {
+        io.to(connections[path][a]).emit("user-joined", socket.id);
+      }
+    });
+
+    socket.on("signal", (toId, message) => {
+      io.to(toId).emit("signal", socket.id, message);
+    });
+
+    socket.on("chat-message", (data, sender) => {});
+
+    socket.on("disconnect", () => {});
+  });
+
   return io;
 };
 
