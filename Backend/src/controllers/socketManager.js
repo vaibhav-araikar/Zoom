4440eab8aaa8926c06ca978ead17203dd1fc2c0a
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-09-21 19:28:16 +0530

    working on landing page

--- Backend/src/controllers/socketManager.js
+++ Backend/src/controllers/socketManager.js
@@ -7,7 +7,14 @@ let messages = {};
 let timeOnline = {};
 
 const connectToSocketServer = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["*"],
+      credentials: true,
+    },
+  });
 
   io.on("connection", (socket) => {
     socket.on("join-call", (path) => {
