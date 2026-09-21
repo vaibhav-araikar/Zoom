a6ae65d3fa7098fb379a1ba0d72080e98baadd5e
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-09-21 20:23:37 +0530

    working on mainpage

--- Frontend/src/pages/landing.jsx
+++ Frontend/src/pages/landing.jsx
@@ -4,12 +4,27 @@ import "../App.css";
 export default function LandingPage() {
   return (
     <div className="landingPageContainer">
+      {/* Navbar */}
       <nav>
         <div className="navHeader">
           <h2>Air Video Call</h2>
         </div>
-        <div className="navList"></div>
+        <div className="navList">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
+          </div>
+        </div>
       </nav>
+
+      {/* Main Page */}
+      <div className="landingMainContainer">
+        <div>
+          <h1>Connect with your loved ones</h1>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
