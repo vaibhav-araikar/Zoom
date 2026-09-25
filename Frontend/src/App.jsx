ae5b523cc32cc86636c45a37f882ff50a9d0ef32
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-09-25 21:00:10 +0530

    authentication data changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,16 +4,19 @@ import LandingPage from "./pages/landing";
 import Authentication from "./pages/authentication";
 import ForgotPassword from "./pages/components/ForgotPassword";
 import SignUp from "./pages/components/SignUp";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/auth" element={<Authentication />}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-        <Route path="/register" element={<SignUp />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<Authentication />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/register" element={<SignUp />}></Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ Frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios, { HttpStatusCode } from "axios";
+
+export const AuthContext = createContext(null);
+
+const clientUrl = axios.create({
+  baseURL: "http://localhost:5000/api/v1/users",
+});
+
+export const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  const router = useNavigate();
+
+  const handleRegister = async (name, username, password) => {
+    try {
+      const request = await clientUrl.post("/register", {
+        name,
+        username,
+        password,
+      });
+
+      if (request.status === HttpStatusCode.Created) {
+        return request.data.message;
+      }
+
+      return request.data;
+    } catch (error) {
+      console.log("Registration error:", error);
+      throw error;
+    }
+  };
+
+  const data = {
+    userData,
+    setUserData,
+    handleRegister,
+    router,
+  };
+
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
